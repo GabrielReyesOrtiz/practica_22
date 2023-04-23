2cd3809def5253c8bfff71d9918a14371f6e34f3
GabrielReyesOrtiz <greyes17@ucol.mx>
2023-04-23 16:08:11 -0600

    Pruebas de vaja negra

--- routes/teachers.js
+++ routes/teachers.js
@@ -53,16 +53,65 @@ router.get('/teacher', function (req, res){
   res.render('teacher');
 });
 //Agregamos nueva ruta por POST para poder agregar un docuento nuevo a nuetsra coleccion
+
+
 router.post('/addTeacher', function(req, res){
-  //nueva instancia de "Person" que recibe valores del body
+  // Validar que el campo idT sea un número de 8 dígitos
+  const regexIdT = /^\d{8}$/;
+  if (!regexIdT.test(req.body.idT)) {
+    return res.status(400).send('El campo idT debe ser un número de 8 dígitos');
+  }
+// validar que tengamos un nombre o mas
+  if (!req.body.name) {
+    return res.status(400).send('El campo "name" es obligatorio');
+  }
+
+    // Validar que el campo id_career tenga exactamente dos apellidos
+const regexIdCareer = /^(\S+ ){1}\S+$/;
+if (!regexIdCareer.test(req.body.id_career)) {
+  return res.status(400).send('El campo id_career debe tener exactamente dos apellidos');
+}
+
+
+// Validar que el campo grado y grupo tenga el formato correcto
+const regexEmail = /^[1-8][A-K]$/;
+if (!regexEmail.test(req.body.email)) {
+  return res.status(400).send('El campo grado y grupo debe tener el formato correcto (ej. 2B) Los grados van de 1 a 8, y los grupos son de la A a la K');
+}
+
+let asistencia = 1; // Valor predeterminado de asistencia
+
+const grado = parseInt(req.body.email.charAt(0)); // Obtener el número de grado como entero
+const grupo = req.body.email.charAt(1); // Obtener la letra de grupo
+
+if (grupo === 'G' && grado === 7) {
+  asistencia = 0; // Si es del grupo 7G, asistencia es 0
+} else {
+  if (grupo === 'A' || grupo === 'B' || grupo === 'C') {
+    asistencia *= 2; // Si es del grupo A, B, o C, asistencia se duplica
+  }
+  if (grado >= 4) {
+    asistencia *= 3; // Si es del grado 4 o mayor, asistencia se triplica
+  }
+}
+
+
+  // Crear una nueva instancia de "Teacher" con los datos del body
   const myTeacher = new Teacher({
     idT: req.body.idT,
     name: req.body.name,
     email: req.body.email,
-    id_career: req.body.id_career}); //Creamos la entidad 
-    myTeacher.save();//guardamos en BD
-    res.redirect('/teachers');//redireccionamos a la vista de personas
-    
+    id_career: req.body.id_career,
+    asistencia: asistencia
+  });
+
+  // Guardar la instancia en la base de datos
+  myTeacher.save(function(err) {
+    if (err) {
+      return res.status(500).send('Error al guardar el registro en la base de datos');
+    }
+    res.redirect('/teachers');
+  });
 });
 
 //Exportamos el router

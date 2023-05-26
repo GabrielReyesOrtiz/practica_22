88837a109b60d6a170849fae3f178cdf813d1539
GabrielReyesOrtiz <greyes17@ucol.mx>
2023-05-26 03:49:23 -0600

    intentonuevo

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(session({
 }));
 
 // Ahora configuramos view engine, agregamos router y el urlencoded
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get("/",(req,res)=>{
